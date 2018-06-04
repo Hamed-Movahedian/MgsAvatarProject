aad57478edbae3db778771c5f2d5551c31d42696
Hamed Movahedian <movahedian.h@gmail.com>
2018-06-04 11:48:01 +0430

    no message

--- Assets/MgsAvatar/Scripts/MgsAvatarEditorWindow.cs
+++ Assets/MgsAvatar/Scripts/MgsAvatarEditorWindow.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using MgsCommonLib.Utilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MgsAvatarEditorWindow : MonoBehaviour
 {
     public float TransitionDuration = 1;
     public RectTransform ImageRoot;
 
-
+    private Image[][] _images;
+    private RectTransform[][] _rectTransforms;
+    
     #region Instance
 
     private static MgsAvatarEditorWindow _instance;
@@ -15,6 +20,58 @@ public class MgsAvatarEditorWindow : MonoBehaviour
     public static MgsAvatarEditorWindow Instance
     {
         get { return _instance ?? FindObjectOfType<MgsAvatarEditorWindow>(); }
+    }
+
+    public int FeatureCount { get { return ImageRoot.childCount; } }
+
+    #endregion
+
+    #region GetRandomAvatarValues
+
+    public List<int> GetRandomAvatarValues()
+    {
+        // initialize results
+        var results = new List<int>();
+
+        // ********** Add random value for each feature
+        foreach (Transform feature in ImageRoot)
+            results.Add(Random.Range(0, feature.childCount));
+
+        return results;
+    }
+
+
+    #endregion
+
+    #region Start - Cache Images and rectTransforms
+
+    public void Start()
+    {
+        // Get images of all features as matrix
+        _images = transform
+            .GetChilds()
+            .Select(child => child.GetComponentsInChildren<Image>(true))
+            .ToArray();
+
+        // Get RectTransform of all images
+        _rectTransforms = _images
+            .Select(images =>
+                images
+                    .Select(image => image.GetComponent<RectTransform>())
+                    .ToArray())
+            .ToArray();
+    }
+
+    #endregion
+
+    #region GetFeature
+
+    public void GetFeature(int featureIndex, int featureValue, RectTransform rectTransform, Image image)
+    {
+        rectTransform.localPosition = _rectTransforms[featureIndex][featureValue].localPosition;
+        rectTransform.localScale = _rectTransforms[featureIndex][featureValue].localScale;
+
+        image.sprite =_images[featureIndex][featureValue].sprite;
     }
 
     #endregion
--- Assets/MgsAvatar/Scripts/MgsAvatarSample.cs
+++ Assets/MgsAvatar/Scripts/MgsAvatarSample.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using MgsCommonLib.Utilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MgsAvatarSample : MonoBehaviour
 {
+    private List<RectTransform> _rectTransforms;
+    private List<Image> _images;
+    private MgsAvatarEditorWindow _avatarWindow;
+
+    #region Editor tools
+
+    #region Initialize 
+
     [ContextMenu("Initialize")]
     public void Initialize()
     {
@@ -25,15 +35,104 @@ public class MgsAvatarSample : MonoBehaviour
         // Copy Images from avatar
         foreach (Transform child in imageRootRT)
         {
-            var newRT = Instantiate(child.gameObject).GetComponent<RectTransform>();
+            // Copy child 
+            GameObject copyGo = Instantiate(child.gameObject);
+
+            // Correct name - remove "(Clone)" part
+            copyGo.name = child.name;
+
+            // Get rect transforms
+            var copyRT = copyGo.GetComponent<RectTransform>();
             var originalRT = child.GetComponent<RectTransform>();
 
-            newRT.SetParent(transform);
+            // Set p
+            copyRT.SetParent(transform);
 
-            newRT.localPosition = originalRT.localPosition;
-            newRT.localScale = originalRT.localScale;
+            // Set rect transform of copy to original
+            copyRT.localPosition = originalRT.localPosition;
+            copyRT.localScale = originalRT.localScale;
+
+            // Delete Extra children so that only one child remains
+            while (copyRT.childCount>1)
+            {
+                //delete first child
+                DestroyImmediate(copyRT.GetChild(0).gameObject);
+            }
+
+            // Enable the only remaining child
+            copyRT.GetChild(0).gameObject.SetActive(true);
+
+            // Set the only child name to name of his parent
+            copyRT.GetChild(0).name = copyRT.name;
         }
 
 
     }
+    #endregion
+
+    #region Set Random avatar
+
+    [ContextMenu("Random Avatar")]
+    public void RandomAvatar()
+    {
+        // Initialize sample and avatar window
+        MgsAvatarEditorWindow.Instance.Start();
+        Start();
+
+        // Get random avatar values from editor
+        var avatarValues = MgsAvatarEditorWindow
+            .Instance
+            .GetRandomAvatarValues();
+
+        // Set avatar values
+        SetFeatureValues(avatarValues);
+    }
+
+    #endregion
+
+    #endregion
+
+
+    #region Start
+
+    void Start()
+    {
+        //******************   Initialize images and rectTransforms
+
+        // Get RectTransform
+        _rectTransforms = transform
+            .GetChilds()                                                    // get first childs
+            .Select(child=>child.GetChild(0).GetComponent<RectTransform>()) // get rectTransform of first child
+            .ToList();
+
+        // Get images
+        _images = transform
+            .GetChilds()    // get first childs
+            .Select(child => child.GetChild(0).GetComponent<Image>()) // get rectTransform of first child
+            .ToList();
+
+        // *************** check Sample validation
+        if (_images.Count != _avatarWindow.FeatureCount)
+        {
+            Debug.LogError("Avatar sample " + name + " is Invalid !! (Initialization required)");
+            return;
+        }
+
+        // **********************  Cache Avatar window image root
+        _avatarWindow = MgsAvatarEditorWindow.Instance;
+    }
+
+    #endregion
+
+    #region SetAvatarValues
+
+    public void SetFeatureValues(List<int> featureValues)
+    {
+        for (int i = 0; i < _avatarWindow.FeatureCount; i++)
+            _avatarWindow.GetFeature(i, featureValues[i], _rectTransforms[i], _images[i]);
+    }
+
+
+    #endregion
+
 }
